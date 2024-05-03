e57a8b504ba4efcba67aed8370b536bed9fda733
Liss <lissetchopper@gmail.com>
2024-05-03 14:03:40 -0600

    Vue js with laravel and Vite auth

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ header }}</div>
+
+                    <div class="card-body">
+                        {{ body }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const header = "Laravel UI + Vue 3"
+const body = "Aplicacion laravel 9 con autenticacion y Vue 3 + Vite.js y Bootstrap 5"
+</script>
